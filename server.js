2eaad639163d3065a03e7af784f50df9f7c797af
shreyxnsh <shreyanshatwork@gmail.com>
2024-04-16 07:01:49 +0530

    added Reciving file indicator

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const net = require('net');
 const fs = require('fs');
 const http = require('http');
 
-const FILE_PATH = '/Users/anand/local-network-data-sharing-using-node/data_file_deeplearing.zip';
+const FILE_PATH = 'data_file_deeplearing.tar.gz';
+// const FILE_PATH = 'readme.txt';
 
 const server = net.createServer((socket) => {
   console.log('Client connected');
@@ -10,6 +11,11 @@ const server = net.createServer((socket) => {
   const writeStream = fs.createWriteStream(FILE_PATH);
 
   socket.pipe(writeStream);
+  console.log('Receving files...'); 
+
+  // To Add code to show time taken to transfer file and time remaining and percentage of file transferred
+  // To Add code to show the speed of transfer in MB/s
+
 
   socket.on('end', () => {
     console.log('File transfer complete');
@@ -20,9 +26,9 @@ const server = net.createServer((socket) => {
     console.error('Error:', err);
     server.close();
   });
-});
+}); 
 
-const ipAddress = "192.168.31.211";
+const ipAddress = "192.168.0.0"; // Add your device IP address here
 const port = 3000;
 
 server.listen(port, ipAddress, () => {
